b0c91d8605c54826a4bef952f17f8ab01de530db
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2022-09-06 16:08:45 -0400

    Update car_prices.py

--- Multiple Linear Regression (with categorical variables)/car_prices.py
+++ Multiple Linear Regression (with categorical variables)/car_prices.py
@@ -1,1 +1,17 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
+df = pd.read_csv(r"C:\Users\nabil\OneDrive\Desktop\Machine Learning\carprices.csv")
+
+dummies = pd.get_dummies(df['Car Model'])
+merged = pd.concat([df, dummies], axis = 1)
+final = merged.drop(['Car Model', 'Mercedez Benz C class'], axis = 1)
+y = final['Sell Price($)']
+final  = final.drop('Sell Price($)', axis = 1)
+
+model = LinearRegression()
+model.fit(final, y)
+
+print(model.score(final, y))
