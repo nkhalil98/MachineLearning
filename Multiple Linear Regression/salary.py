21bb2360ac84ea34cea4219cf3bdf16c0fc7121e
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2022-10-01 14:37:01 -0400

    Update salary.py

--- Multiple Linear Regression/salary.py
+++ Multiple Linear Regression/salary.py
@@ -7,7 +7,8 @@ df = pd.read_csv(r"C:\Users\nabil\OneDrive\Desktop\Machine Learning\hiring.csv")
 
 df.experience = df.experience.apply(lambda x: text2num(x, 'en') if type(x) is str else x)
 df.experience = df.experience.fillna(0)
-df['test_score(out of 10)'] = df['test_score(out of 10)'].fillna(math.floor(df['test_score(out of 10)'].median()))
+exp_median = math.floor(df['test_score(out of 10)'].median())
+df['test_score(out of 10)'] = df['test_score(out of 10)'].fillna(exp_median)
 
 model = linear_model.LinearRegression()
 model.fit(df[['experience', 'test_score(out of 10)', 'interview_score(out of 10)']], df['salary($)'])
